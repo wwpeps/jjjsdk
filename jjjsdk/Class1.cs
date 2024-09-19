f16c069d9a781e6756c5651529a4c4b0156d60d8
wwpeps <aboba1333333@gmail.com>
2024-09-19 15:47:46 +0500

    Добавьте файлы проекта.

---
+++ jjjsdk/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jjjsdk
+{
+    internal class Class1
+    {
+        private int id;
+        private string name
+    }
+}
